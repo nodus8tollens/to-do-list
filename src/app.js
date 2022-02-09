26e9c6656029416ad46889e1acbee1d231ad1cb9
nodus8tollens <mladenovskipredrag@gmail.com>
2022-02-09 13:01:38 +0100

    Some progress

--- src/app.js
+++ src/app.js
@@ -5,9 +5,9 @@ At a minimum to-do items should have a title, description, dueDate and priority.
 You might also want to include notes or even a checklist.
  */
 
-export function newTask(title, date, description, priority){
-    tasks.push({title, date, description, priority});
+export function newTask(title, date, description, priority, id){
+    tasks.push({title, date, description, priority, id});
     console.log(tasks)
 }
 
-export const tasks = [];
+export let tasks = [];
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ const title = document.getElementById("title");
 const date = document.getElementById("date");
 const description = document.getElementById("description");
 const priority = document.getElementById("priority");
+
 function clearPopUp(){
     title.value = "";
     date.value = "";
@@ -27,7 +28,8 @@ addTaskBtn.addEventListener('click', popUpActive);
 const popUpBoxAddBtn = document.querySelector('#pop-up-box-add');
 popUpBoxAddBtn.addEventListener('click', () => {
     popUpNotActive();
-    newTask(title.value, date.value, description.value, priority.value);
+    newTask(title.value, date.value, description.value, priority.value, Date.now());
+    displayTasks();
     clearPopUp();
 })
 
@@ -36,3 +38,61 @@ popUpBoxCancelBtn.addEventListener('click', () => {
     popUpNotActive();
     clearPopUp();
 });
+
+function removeChildren(parent){
+    while(parent.lastChild){
+        parent.removeChild(parent.lastChild);
+    }
+}
+
+const content = document.getElementById('content');
+
+function createTaskDivs(element){
+    const div = document.createElement('div');
+    div.setAttribute('data-index', element.id);
+    div.style.display = 'flex';
+    div.style.justifyContent = 'space-between'
+
+    const titleDiv = document.createElement('div');
+    titleDiv.innerText = element.title;
+    div.appendChild(titleDiv);
+
+    const priorityDiv = document.createElement('div');
+    priorityDiv.innerText = element.priority;
+    div.appendChild(priorityDiv);
+
+    const dateDiv = document.createElement('div');
+    dateDiv.innerText = element.date;
+    div.appendChild(dateDiv);
+
+    const detailsBtn = document.createElement('button');
+    detailsBtn.innerText = 'Details';
+    detailsBtn.addEventListener('click', (e) => {
+        document.getElementById('info-box-overlay').style.display = 'block';
+        document.getElementById('info-box').setAttribute('data-index', e.target.parentNode.getAttribute('data-index'));
+        document.getElementById('info-box-title').innerText = 'Title';
+        document.getElementById('info-box-date').innerText = 'Date';
+        document.getElementById('info-box-description').innerText = 'Description';
+        document.getElementById('info-box-priority').innerText = 'Priority';
+    });
+    div.appendChild(detailsBtn);
+
+    content.appendChild(div);
+}
+
+document.getElementById('info-box-cancel').addEventListener('click', () => {
+    document.getElementById('info-box-overlay').style.display = 'none';
+})
+
+document.getElementById('info-box-remove').addEventListener('click', (e) => {
+    for(let i = 0; i < tasks.length; i++){
+        if(parseInt(e.target.parentNode.parentNode.getAttribute('data-index')) === tasks[i].id) tasks.splice(i, 1);
+    }
+    displayTasks();
+    document.getElementById('info-box-overlay').style.display = 'none';
+})
+
+function displayTasks() {
+    removeChildren(content);
+    tasks.forEach(element => createTaskDivs(element));
+}
